5834e8186eaa608427bc2399f77603958197844f
Eugene <tsurcan@softreactor.com>
2012-10-21 14:07:20 +0300

    First pages commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*
+ * Author: Eugene Tsurcan
+ * email: tsurcan@softreactor.com
+ */
+
+var spotTheBall = spotTheBall || {};
+
+spotTheBall.init = function() {
+  //
+};
+
+// Document Ready
+$(function($) {
+  spotTheBall.init();
+});
+
+
+
